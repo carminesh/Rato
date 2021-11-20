bf8852c6481355dbdaedabd92545ffb935b577fb
carminesh <carmineshoot5@outlook.it>
2021-11-20 18:20:30 +0100

    Rato - Footer update

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,7 +1,12 @@
 <template>
-  
-  <img id="footer" src="../assets/footer.svg" alt="footer image">
-  <img id="mobile-footer" src="../assets/mobileFooter.svg" alt="footer image">
+
+
+  <div class="footer-container">
+    <div class="footer">
+        <h3>@Carmine Fabbri 2021 - 2022</h3>
+        <a href="https://github.com/carminesh/Rato"><i class="fab fa-github-square"></i></a>
+    </div>
+  </div>
 
 </template>
 
@@ -15,8 +20,37 @@ export default {
 
 <style scoped>
 
+  .footer-container {
+    color: rgb(240, 240, 240);
+    position: absolute;
+    bottom: -100vh;
+    left: 0;
+    width: 100%;
+    height: 15%;
+    max-height: 15%;
+    background-color: #1d1c1e;
+  }
 
-  #footer {
+  .footer {
+    margin: auto;
+    height: 100%;
+    width: 80%;
+    display: flex;
+    align-items: center;
+  }
+
+  .footer a {
+    text-decoration: none;
+  }
+
+  i {
+    color: white;
+    padding-left: 20px;
+    font-size: 3rem;
+    text-decoration: none;
+  }
+
+  /* #footer {
     pointer-events: none;
     bottom: 0;
     left: 0;
@@ -27,73 +61,53 @@ export default {
 
   #mobile-footer { 
     display: none;
-  }
+  } */
 
   /* MEDIA QUERIES */
 
   /*Mobile devices*/
   @media screen and (max-width: 480px) {
 
-    #mobile-footer {
-      display: none;
+    .footer-container {
+      height: 10%;
+      max-height: 10%;
     }
-
-    #footer {
-      display: none
-    }
+    
 
   }
 
    /*iPads, Tablets devices*/
   @media screen and (min-width: 481px) and (max-width: 768px) {
 
-      #footer {
-        position: absolute;
-        pointer-events: none;
-        bottom: -100vh;
-        left: 0;
-        object-fit: cover;
-        z-index: 1;
-        width: 100%;
-      }
+    .footer-container {
+      height: 10%;
+      max-height: 10%;
+    }
+
   }
 
   /*Small screens, laptops*/
   @media screen and (min-width: 769px) and (max-width: 1324px)  {
-     #footer {
-        position: absolute;
-        pointer-events: none;
-        bottom: -105vh;
-        left: 0;
-        object-fit: cover;
-        z-index: 1;
-        width: 100%;
-      }
+
+    .footer-container {
+      height: 10%;
+      max-height: 10%;
+    }
   }
 
   /*Large screen*/
   @media screen and (min-width: 1025px) and (max-width:1400px) {
-    #footer {
-        position: absolute;
-        pointer-events: none;
-        bottom: -105vh;
-        left: 0;
-        object-fit: cover;
-        z-index: 1;
-        width: 100%;
-      }
+    .footer-container {
+      height: 12%;
+      max-height: 12%;
+    }
   }
   
   @media screen and (min-width: 1401px) {
-    #footer {
-        position: absolute;
-        pointer-events: none;
-        bottom: -105vh;
-        left: 0;
-        object-fit: cover;
-        z-index: 1;
-        width: 100%;
-      }
+    .footer-container {
+      height: 12%;
+      max-height: 12%;
+    }
   }
 
 </style>
